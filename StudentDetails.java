f208a23b7c394cf4cf83adc24ff4ea7beb68a083
rahimathulla08 <168528448+rahimathulla08@users.noreply.github.com>
2025-07-22 23:05:37 +0530

    Add files via upload

---
+++ StudentDetails.java
@@ -0,0 +1,13 @@
+public class StudentDetails {
+    public static void main(String[] args) {
+        
+        String name = "Rahimathulla";
+        String rollNo = "2451-22-748-303";
+        String department = "Computer Science";
+
+        System.out.println("Student Details:");
+        System.out.println("Name: " + name);
+        System.out.println("Roll No: " + rollNo);
+        System.out.println("Department: " + department);
+    }
+}
